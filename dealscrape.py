e0bfaee7369b83c32dc11455172c958e4292c271
delton <hulbert.delton@gmail.com>
2023-12-27 22:32:58 -0700

    works locally, not on server

--- dealscrape.py
+++ dealscrape.py
@@ -1,43 +1,69 @@
 import requests
 from bs4 import BeautifulSoup
-#import datetime
-from datetime import datetime, timedelta
 
-musiciansfriendurl = "https://www.musiciansfriend.com/stupid"
-channelid = 1170568462918221874
+MUSICIANS_FRIEND_URL = "https://www.musiciansfriend.com/stupid"
 
-def GetDealInformation():
-    page = requests.get(musiciansfriendurl)
+def __GetDealInformation():
+    page = requests.get(MUSICIANS_FRIEND_URL)
     websoup = BeautifulSoup(page.content, "html.parser")
     primary = websoup.find(id = "primaryAvailability")
     return primary
 
 def GetDealOfTheDayTitle():
-    newdeal = GetDealInformation()
+    newdeal = __GetDealInformation()
     dealname = newdeal.find(class_ = "displayNameColor").text.strip()
-    strout = "__**MUSICIAN'S FRIEND STUPID DEAL OF THE DAY**__\n" + dealname
-    return strout
+    return "**__" + dealname + "__**"
 
 def GetDealOfTheDayImage():
-    newdeal = GetDealInformation()
+    newdeal = __GetDealInformation()
     dealimage = newdeal.find(class_ = "sdBold").find('img').get('src')
     return dealimage
 
 def GetDealOfTheDayText():
-    newdeal = GetDealInformation()
+    newdeal = __GetDealInformation()
     regularprice = newdeal.find(class_ = "regular-price").text.strip()
     savings = newdeal.find(class_ = "feature-save").find(class_ = "formatted-price").text.strip()
     dealprice = newdeal.find(class_ = "feature-price").find(class_ = "formatted-price").text.strip()
     description = newdeal.find(class_ = "feature-description").text.strip()
     enddate = newdeal.find(class_ = "feature-availability").find("strong").text.strip()
-    strout = "\n" + description  + "\n\n"
-    strout += "__Regular Price__: " + regularprice + "\n"
-    strout += "__Savings__: " + savings  + "\n"
-    strout += "__Deal Price__: " + dealprice + "\n"
-    strout += enddate + "\n\n"
-    strout += "**__GET IT HERE__**: " + musiciansfriendurl
-    return strout
+    strout = ["\n" + description  + "\n\n"]
+    strout[0] += "__Regular Price__: " + regularprice + "\n"
+    strout[0] += "__Savings__: " + savings  + "\n"
+    strout[0] += "__Deal Price__: " + dealprice + "\n"
+    strout[0] += enddate + "\n\n"
+    strout[0] += "**__GET IT HERE__**: " + MUSICIANS_FRIEND_URL
+
+    if len(strout[0]) > 2000:
+        strout[0] = description.split('\n')
+        strout_start = ""
+        strout_mid = ""
+        i = 0
+        strout_s = ""
+        while i < len(strout[0]) and len(strout_start) < 2000:
+            if strout[0][i] == '' and strout[0][i - 1] != '':
+                strout_s += "\n"
+            else:
+                n = strout[0][i]
+                strout_s += "- " + strout[0][i] + '\n'
+            i += 1
+        strout_start = strout_s[:len(strout_start) - len(strout[0][i - 1]) - 4]
+        i -= 1
+        while i < len(strout[0]):
+            if strout[0][i] == '' and strout[0][i - 1] != '':
+                strout_mid += "\n"
+            else:
+                m = strout[0][i]
+                strout_mid += "- " + strout[0][i] + '\n'
+            i += 1
+        
+        strout_end = "__Regular Price__: " + regularprice + "\n" + "__Savings__: " + savings  + "\n" + enddate + "\n\n" + "**__GET IT HERE__**: " + MUSICIANS_FRIEND_URL
+        strout = [strout_start, strout_mid, strout_end]
+
+    return strout_end
 
 #print(GetDealOfTheDayTitle())
 #print(GetDealOfTheDayImage())
 #print(GetDealOfTheDayText())
+#d = GetDealOfTheDayText()
+#for s in d:
+#    print(s)
--- main.py
+++ main.py
@@ -1,5 +1,35 @@
-import asyncio, discord
-from discord.ext import commands
+import interactions
+from interactions import Task, IntervalTrigger, slash_command, SlashContext
 from decouple import config
-import DealOfTheDay
+import dealscrape
 
+TOKEN = config('TOKEN')
+DEALTIME = '10:30' #fix this once you learn how to do static times
+
+dealbot = interactions.Client(token = TOKEN)
+
+@slash_command(
+        name = 'post_stupid_deal',
+        description = "post the musician's friend stupid deal of the day"
+        )
+async def PostStupidDeal(ctx: SlashContext):
+    print("Posting Stupid Deal of the Day\n")
+    print(dealscrape.GetDealOfTheDayTitle())
+    
+    info = dealscrape.GetDealOfTheDayTitle()
+    deal_info = dealscrape.GetDealOfTheDayText()
+    try:
+        if type(deal_info) is list:
+            for d in deal_info:
+                if len(d) > 0:
+                    info += "\n" + d
+        else:
+            info += "\n" + (deal_info)
+    except:
+        info += "\n" + ("For more info, visit " + dealscrape.MUSICIANS_FRIEND_URL)
+    
+    await ctx.send(info)
+    await ctx.send(dealscrape.GetDealOfTheDayImage())
+    return
+
+dealbot.start()
